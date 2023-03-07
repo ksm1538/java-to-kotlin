152d43e275436cedf3ae66996dc27efb95847ee0
smk <ksm1538@naver.com>
2023-03-07 20:38:02 +0900

    코틀린 enum class 공부

--- src/main/kotlin/lecture14/Month.kt
+++ src/main/kotlin/lecture14/Month.kt
@@ -1,5 +1,27 @@
 package lecture14
 
+fun handleMonth(month: Month) {
+    // when 을 이용해서 간편하게 Enum 을 사용할 수 있다.
+    when(month) {
+
+        Month.JANUARY -> {
+            println("1월입니다")
+        }
+
+        Month.FEBRUARY -> {
+            println("2월입니다")
+        }
+
+        Month.MARCH -> {
+            println("3월입니다")
+        }
+
+    }
+}
 enum class Month(
-    //private val name: String
-)
+    private val code: String
+){
+    JANUARY("Jan"),
+    FEBRUARY("Feb"),
+    MARCH("Mar");
+}
